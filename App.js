0e5b0b4e1f8abefd9388575ec39f00d2c7359a85
aaronsiim <web3aaron@gmail.com>
2022-09-27 18:17:39 -0400

    final commit with multiple pages UI

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import BasketScreen from './screens/BasketScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,12 +16,18 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen}/> 
-            <Stack.Screen name="Restaurant" component={RestaurantScreen}/> 
-        </Stack.Navigator>
-      </TailwindProvider>
+      <Provider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+              <Stack.Screen name="Home" component={HomeScreen}/> 
+              <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
+              <Stack.Screen name="Basket" 
+                component={BasketScreen}
+                options={{ presentation: "modal", headerShown: false }}
+              />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
